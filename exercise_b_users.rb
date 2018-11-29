9dde18d4cbb9227ac26939e0f8b8621302b21402
Stephen Rafferty <srafferty73@gmail.com>
2018-11-29 09:55:38 +0000

    ex a 2

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,6 +55,8 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+puts users[:Jonathan][:twitter]
+
 # 2. Get Erik's hometown
 # 3. Get the array of Erik's lottery numbers
 # 4. Get the type of Avril's pet Monty
